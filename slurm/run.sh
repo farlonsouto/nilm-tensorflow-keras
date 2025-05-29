6ab2d90c482008a7e1ae270f5a553bf5e16a5cdf
Farlon Souto <farlonsouto@gmail.com>
2025-05-29 18:08:07 +0200

    Removes risk of hyperparam overwriting by passing params via scripts ref:AIVR2025

--- slurm/run.sh
+++ slurm/run.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # Usage check
-if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 <ARCH> <PURPOSE> <APPLIANCE> <KEY>"
-    echo "Example: $0 bert train 'dish washer' simpleLossFn"
+if [ "$#" -ne 7 ]; then
+    echo "Usage: $0 <PURPOSE> <ARCH> <APPLIANCE> <REGULARIZER> <CONTINUATION> <MASK> <KEY>"
+    echo "Example: $0 bert train 'dish washer' l1l2 first unmasked G2"
     exit 1
 fi
 
 PURPOSE=$1
 ARCH=$2
 APPLIANCE=$3
-KEY=$4
+REGULARIZER=$4
+IS_CONTINUATION=$5
+MASKED=$6
+KEY=$7
 
 # Sanitize appliance name for filename usage
 SAFE_APPLIANCE=$(echo "$APPLIANCE" | tr ' ' '_' | tr '[:upper:]' '[:lower:]')
@@ -21,7 +24,7 @@ LOGFILE="out_${ARCH}_${SAFE_APPLIANCE}_${KEY}.log"
 
 # Submit the job
 sbatch \
-  --export=ALL,ARCH=$ARCH,PURPOSE=$PURPOSE,APPLIANCE="$APPLIANCE" \
+  --export=ALL,ARCH="$ARCH",PURPOSE="$PURPOSE",APPLIANCE="$APPLIANCE",REGULARIZER="$REGULARIZER",IS_CONTINUATION="$IS_CONTINUATION",MASKED="$MASKED" \
   --job-name="$JOBNAME" \
   --output="$LOGFILE" \
   executor.job
